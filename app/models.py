72718fafe5441f9f7a7d07443463b93584f259c6
Michael Salvatore <msalvatore22@gmail.com>
2022-10-19 15:46:26 -0400

    Created app, made migrations, put sensitive info in environment variables

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
 
+# Create your models here.
 class EspnPassingStats(models.Model):
+  class Meta:
+    db_table = 'espn_passing_stats'
   player_full_name = models.CharField(max_length=50)
   team_full = models.CharField(max_length=30)
   team_abrv = models.CharField(max_length=3)
@@ -9,18 +11,41 @@ class EspnPassingStats(models.Model):
   gp = models.SmallIntegerField()
   cmp = models.SmallIntegerField()
   att = models.SmallIntegerField()
-  cmp_percent = models.DecimalField()
+  cmp_percent = models.DecimalField(max_digits=5, decimal_places=2)
   yds = models.SmallIntegerField()
-  avg = models.DecimalField()
-  yds_g = models.DecimalField()
+  avg = models.DecimalField(max_digits=5, decimal_places=2)
+  yds_g = models.DecimalField(max_digits=5, decimal_places=2)
   lng = models.SmallIntegerField()
   td = models.SmallIntegerField()
   int = models.SmallIntegerField()
   sack = models.SmallIntegerField()
   syl = models.SmallIntegerField()
-  rtg = models.DecimalField()
-  
+  rtg = models.DecimalField(max_digits=5, decimal_places=2)
+
+class EspnReceivingStats(models.Model):
+  class Meta:
+    db_table = 'espn_receiving_stats'
+  player_full_name = models.CharField(max_length=50)
+  team_full = models.CharField(max_length=30)
+  team_abrv = models.CharField(max_length=3)
+  season = models.CharField(max_length=25)
+  gp = models.SmallIntegerField()
+  rec = models.SmallIntegerField()
+  tgts = models.SmallIntegerField()
+  yds = models.SmallIntegerField()
+  avg = models.DecimalField(max_digits=5, decimal_places=2)
+  yds_g = models.DecimalField(max_digits=5, decimal_places=2)
+  lng = models.SmallIntegerField()
+  td = models.SmallIntegerField()
+  big = models.SmallIntegerField()
+  fum = models.SmallIntegerField()
+  lst = models.SmallIntegerField()
+  yac = models.SmallIntegerField()
+  fd = models.SmallIntegerField()
+
 class EspnRushingStats(models.Model):
+  class Meta:
+    db_table = 'espn_rushing_stats'
   player_full_name = models.CharField(max_length=50)
   team_full = models.CharField(max_length=30)
   team_abrv = models.CharField(max_length=3)
@@ -28,16 +53,18 @@ class EspnRushingStats(models.Model):
   gp = models.SmallIntegerField()
   att = models.SmallIntegerField()
   yds = models.SmallIntegerField()
-  avg = models.DecimalField()
+  avg = models.DecimalField(max_digits=5, decimal_places=2)
   lng = models.SmallIntegerField()
   big = models.SmallIntegerField()
   td = models.SmallIntegerField()
-  yds_g = models.DecimalField()
+  yds_g = models.DecimalField(max_digits=5, decimal_places=2)
   fum = models.SmallIntegerField()
   lst = models.SmallIntegerField()
   fd = models.SmallIntegerField()
   
 class EspnDefenseStats(models.Model):
+  class Meta:
+    db_table = 'espn_defense_stats'
   player_full_name = models.CharField(max_length=50)
   team_full = models.CharField(max_length=30)
   team_abrv = models.CharField(max_length=3)
@@ -46,10 +73,10 @@ class EspnDefenseStats(models.Model):
   solo = models.SmallIntegerField()
   ast = models.SmallIntegerField()
   tot = models.SmallIntegerField()
-  sack = models.DecimalField()
+  sack = models.DecimalField(max_digits=4, decimal_places=2)
   sack_yds = models.SmallIntegerField()
   pd = models.SmallIntegerField()
-  int = models.DecimalField()
+  int = models.DecimalField(max_digits=4, decimal_places=2)
   yds = models.SmallIntegerField()
   lng = models.SmallIntegerField()
   td = models.SmallIntegerField()
@@ -57,4 +84,3 @@ class EspnDefenseStats(models.Model):
   fr = models.SmallIntegerField()
   ftd = models.SmallIntegerField()
   kb = models.SmallIntegerField()
-  
